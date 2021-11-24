65a800cf2dba1e45b9ab78966f4b5209f16ff5c6
FloareDeMai <matei.g.florentina@gmail.com>
2021-11-24 12:11:38 +0200

    added logo and header to Navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { AppBar, Typography, Toolbar, Avatar, Button } from "@mui/material";
 
+
+import logo from "../../images/logo.png"
 import useStyles from "./styles";
 
 function Navbar() {
+    const classes = useStyles();
     return (
         <>
-            Navbar
+           <AppBar>
+               <div>
+                   <img src={logo} alt="logo" height="60"/>
+                   <Typography variant="h2" align="center">
+                       Events
+                   </Typography>
+               </div>
+           </AppBar>
         </>
     )
 }
