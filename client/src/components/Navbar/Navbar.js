a0f70eba2fd21246dd369712db3507116467e73d
FloareDeMai <matei.g.florentina@gmail.com>
2021-11-24 11:53:27 +0200

    client project setup

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import useStyles from "./styles";
+
+function Navbar() {
+    return (
+        <>
+            Navbar
+        </>
+    )
+}
+
+export default Navbar;
