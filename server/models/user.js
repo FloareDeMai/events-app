8467151808fe7002f4571128b81dcf10e1ab0b1b
FloareDeMai <matei.g.florentina@gmail.com>
2021-11-25 10:28:44 +0200

    created schema for user

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import uniqueValidator from 'mongoose-unique-validator';
+const Schema = mongoose.Schema;
+
+const userSchema =  new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, minLength: 6},
+    image:{type: String},
+    events: [{type: mongoose.Types.ObjectId, required: true, ref: 'Event'}]
+})
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
