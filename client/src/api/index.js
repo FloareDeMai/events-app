bb3a8acbba39bb287c44aa3fb652c151bdf74f27
FloareDeMai <matei.g.florentina@gmail.com>
2021-11-26 21:25:22 +0200

    added .env for client side

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5006/api'});
+const API = axios.create({baseURL: process.env.REACT_APP_BASE_URL});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('user')) {
